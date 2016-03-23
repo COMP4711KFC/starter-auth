86410f22e9b90d3344462eeadbea3ed71f15e7e1
cleed <a00914442@SE12L0320-13.ad.bcit.ca>
2016-03-23 10:17:45 -0700

    login php

---
+++ application/views/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form name="login" method="post" action="/auth/submit">
+            UserID: <input type="text" name="userid"></input><br/>
+            Password: <input type="password" name="password"></input><br/>
+            <input type="submit">Submit</input>
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
